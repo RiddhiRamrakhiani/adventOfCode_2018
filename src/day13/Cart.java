840a5b8a3680ba2c8b5de19cfea234afd4618382
riddhi.ramrakhiani <riddhi.ramrakhiani@tacton.com>
2018-12-18 07:04:26 +0100

    Day13_part2

--- src/day13/Cart.java
+++ src/day13/Cart.java
@@ -5,32 +5,11 @@ public class Cart {
     int row;
     int column;
     char nextJunctionMove='L';
-    int oldX;
-    int oldY;
-    boolean processedInthispass=false;
+
    char symbol;
    boolean collided=false;
 
-   /* @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof Cart){
-            Cart anotherCart = (Cart)obj;
-            if(anotherCart.row == this.row && anotherCart.column== this.column )
-                return true;
-        }
-        return false;
-    }
-    @Override
-    public int hashCode(){
-
-        int prime = 31;
-        int hashcode = 1;
-       // hashcode = row.hashCode()+"-".hashCode()+column.hashCode();
-        hashcode = prime * hashcode + row;
-        hashcode = prime * hashcode + column;
-
-        return hashcode;
-    }*/
+  
 
 public int compareTo(Cart cart){
 
